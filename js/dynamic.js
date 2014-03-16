cfa1ebc5141d9bebecd9d6b0ad06d57b8ffcc80f
Michael B <webmikez@gmail.com>
2014-03-16 16:00:25 +0000

    Fixed datapicker uses

--- js/dynamic.js
+++ js/dynamic.js
@@ -377,24 +377,26 @@ $(document).ready(function() {
 		$(this).parent().fadeOut(0);
 		return false;
 	});
-	$.datepicker.regional['ru'] = {
-		closeText: 'Закрыть', 
-		prevText: '&#x3c;Пред', 
-		nextText: 'След&#x3e;', 
-		currentText: 'Сегодня', 
-		monthNames: ['Январь','Февраль','Март','Апрель','Май','Июнь', 
-		'Июль','Август','Сентябрь','Октябрь','Ноябрь','Декабрь'], 
-		monthNamesShort: ['Янв','Фев','Мар','Апр','Май','Июн', 
-		'Июл','Авг','Сен','Окт','Ноя','Дек'], 
-		dayNames: ['воскресенье','понедельник','вторник','среда','четверг','пятница','суббота'], 
-		dayNamesShort: ['вск','пнд','втр','срд','чтв','птн','сбт'], 
-		dayNamesMin: ['Вс','Пн','Вт','Ср','Чт','Пт','Сб'], 
-		dateFormat: 'dd.mm.yy', 
-		firstDay: 1, 
-		isRTL: false 
-	}; 
-	$.datepicker.setDefaults($.datepicker.regional['ru']); 
-	$('div.calendar input.data').datepicker();
+    if($('div.calendar input.data').length) {
+        $.datepicker.regional['ru'] = {
+            closeText: 'Закрыть',
+            prevText: '&#x3c;Пред',
+            nextText: 'След&#x3e;',
+            currentText: 'Сегодня',
+            monthNames: ['Январь','Февраль','Март','Апрель','Май','Июнь',
+            'Июль','Август','Сентябрь','Октябрь','Ноябрь','Декабрь'],
+            monthNamesShort: ['Янв','Фев','Мар','Апр','Май','Июн',
+            'Июл','Авг','Сен','Окт','Ноя','Дек'],
+            dayNames: ['воскресенье','понедельник','вторник','среда','четверг','пятница','суббота'],
+            dayNamesShort: ['вск','пнд','втр','срд','чтв','птн','сбт'],
+            dayNamesMin: ['Вс','Пн','Вт','Ср','Чт','Пт','Сб'],
+            dateFormat: 'dd.mm.yy',
+            firstDay: 1,
+            isRTL: false
+        };
+        $.datepicker.setDefaults($.datepicker.regional['ru']);
+        $('div.calendar input.data').datepicker();
+    }
 	$('.taskadd > div > div p:first-child').each(function() {
 		var ph = $(this).height();
 		if (ph > 27) {
