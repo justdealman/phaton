537b79d1ecd34effbd6dc68403c98e762d488da8
Alexandr <justdealman@gmail.com>
2014-09-08 03:49:50 +0700

    jquery ui dialog + datapicker

--- js/dynamic.js
+++ js/dynamic.js
@@ -117,6 +117,25 @@ $(window).load(function(){
 	});*/
 });
 $(document).ready(function() {
+    $('#dialog').dialog({
+		autoOpen: false,
+		width: 357,
+		resizable: false
+    });
+    $('#opener').click(function() {
+		$('#dialog').dialog('open');
+		return false;
+    });
+    $('.ui-dialog .ui-dialog-content .controls .cancel a').click(function() {
+		$(this).parents('.ui-dialog-content').dialog('close');
+		return false;
+    });
+	$('#data').datepicker({
+		dateFormat: 'dd.mm.yy',
+		firstDay: 1,
+		dayNamesMin: [ 'Вс', 'Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб' ],
+		monthNames: [ 'Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь' ]
+	});
 	if ( $('body.chat').length > 0 ) {
 		chat();
 	}
